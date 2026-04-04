8cbde78f6cc535a6d22f75a1357963d1755e6654
shobuzako-kensuke <134984432+shobuzako-kensuke@users.noreply.github.com>
2026-04-04 10:14:50 +0000

    Deploying to gh-pages from @ shobuzako-kensuke/shobuzako-kensuke.github.io@84d6db9576c144ff03d77d923554e80106227657 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,91 +3,98 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-home",
-    title: "Home",
+    id: "nav-ホーム",
+    title: "ホーム",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-profile",
-          title: "Profile",
-          description: "プロフィール",
+  },{id: "nav-プロフィール",
+          title: "プロフィール",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/profile/";
           },
-        },{id: "dropdown-projects",
-              title: "Projects",
+        },{id: "dropdown-研究内容",
+              title: "研究内容",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/research/projects/";
               },
-            },{id: "dropdown-publications",
-              title: "Publications",
+            },{id: "dropdown-出版物リスト",
+              title: "出版物リスト",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/research/publications/";
               },
-            },{id: "dropdown-presentations",
-              title: "Presentations",
+            },{id: "dropdown-発表リスト",
+              title: "発表リスト",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/research/presentations/";
               },
-            },{id: "dropdown-software",
-              title: "Software",
+            },{id: "dropdown-ソフトウェア",
+              title: "ソフトウェア",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/works/software/";
               },
-            },{id: "dropdown-articles",
-              title: "Articles",
+            },{id: "dropdown-web記事",
+              title: "Web記事",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/works/articles/";
               },
-            },{id: "dropdown-research-notes",
-              title: "Research Notes",
+            },{id: "dropdown-ノート",
+              title: "ノート",
               description: "",
               section: "Dropdown",
               handler: () => {
                 window.location.href = "/works/research_notes/";
               },
-            },{id: "nav-news",
-          title: "News",
-          description: "過去のお知らせ",
+            },{id: "nav-ニュース",
+          title: "ニュース",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/news/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
+        },{id: "nav-ブログ",
+          title: "ブログ",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-輪読の記録",
+        },{id: "nav-english",
+          title: "English",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/en/";
+          },
+        },{id: "post-教科書読みの記録",
         
-          title: "輪読の記録",
+          title: "教科書読みの記録",
         
-        description: "これまでに輪読した本たち",
+        description: "これまでに読んだ教科書",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/rindoku/";
+            window.location.href = "/blog/2025/books/";
           
         },
-      },{id: "post-合唱の記録",
+      },{id: "post-演奏会への出演歴",
         
-          title: "合唱の記録",
+          title: "演奏会への出演歴",
         
-        description: "出演した演奏会",
+        description: "合唱団員として舞台に立った主な演奏会",
         section: "Posts",
         handler: () => {
           
@@ -99,41 +106,46 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-研究費採択-九州大学-次世代研究者挑戦的研究プログラム-院生融合研究プロジェクト-に採択されました",
-          title: '【研究費採択】九州大学 次世代研究者挑戦的研究プログラム「院生融合研究プロジェクト」に採択されました',
+            },},{id: "news-九州大学-次世代研究者挑戦的研究プログラム-院生融合研究プロジェクト-に採択されました",
+          title: '九州大学 次世代研究者挑戦的研究プログラム「院生融合研究プロジェクト」に採択されました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-04-07-grant/";
-            },},{id: "news-学会発表-日本地球惑星科学連合2025年大会-にて口頭発表を行いました",
-          title: '【学会発表】「日本地球惑星科学連合2025年大会」にて口頭発表を行いました',
+            },},{id: "news-日本地球惑星科学連合-2025年大会にて口頭発表を行いました",
+          title: '日本地球惑星科学連合 2025年大会にて口頭発表を行いました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-05-27-presentation/";
-            },},{id: "news-学会発表-第30回-計算工学講演会-にて口頭発表を行いました",
-          title: '【学会発表】「第30回 計算工学講演会」にて口頭発表を行いました',
+            },},{id: "news-第30回-計算工学講演会にて口頭発表を行いました",
+          title: '第30回 計算工学講演会にて口頭発表を行いました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-06-05-presentation/";
-            },},{id: "news-論文掲載-results-in-applied-mathematics-に論文が掲載されました",
-          title: '【論文掲載】Results in Applied Mathematics に論文が掲載されました',
+            },},{id: "news-results-in-applied-mathematics-に論文が掲載されました",
+          title: 'Results in Applied Mathematics に論文が掲載されました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-06-13-paper/";
-            },},{id: "news-受賞-日本地球惑星科学連合2025年大会-にて学生優秀発表賞を受賞しました",
-          title: '【受賞】「日本地球惑星科学連合2025年大会」にて学生優秀発表賞を受賞しました',
+            },},{id: "news-日本地球惑星科学連合-2025年大会にて学生優秀発表賞を受賞しました",
+          title: '日本地球惑星科学連合 2025年大会にて学生優秀発表賞を受賞しました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-07-11-award/";
-            },},{id: "news-セミナー発表-惑星科学研究センター-cps-にて口頭発表を行いました",
-          title: '【セミナー発表】惑星科学研究センター（CPS）にて口頭発表を行いました',
+            },},{id: "news-惑星科学研究センターにて口頭発表-招待講演-を行いました",
+          title: '惑星科学研究センターにて口頭発表（招待講演）を行いました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-07-14-seminar/";
-            },},{id: "news-お知らせ-ウェブサイトをリニューアルしました",
-          title: '【お知らせ】ウェブサイトをリニューアルしました',
+            },},{id: "news-ウェブサイトを全面的にリニューアルしました",
+          title: 'ウェブサイトを全面的にリニューアルしました',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/2025-10-14-website/";
+              window.location.href = "/news/2026-03-15-website/";
+            },},{id: "news-惑星深部研究会-2026福岡にて口頭発表を行いました",
+          title: '惑星深部研究会 2026福岡にて口頭発表を行いました',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2026-03-18-seminar/";
             },},{id: "projects-高精度sph法",
           title: '高精度SPH法',
           description: "最小二乗SPH (LS-SPH) の開発",
