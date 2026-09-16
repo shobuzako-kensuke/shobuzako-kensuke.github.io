97b405ffbd2bb0e329061f52a3518e97429fe4fe
shobuzako-kensuke <134984432+shobuzako-kensuke@users.noreply.github.com>
2026-09-16 04:04:44 +0000

    Deploying to gh-pages from @ shobuzako-kensuke/shobuzako-kensuke.github.io@249dc29b288396cac3fc8081bda6754e6a538de4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -151,8 +151,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-05-29-presentation/";
-            },},{id: "news-第31回-計算工学講演会にて口頭発表を行いました",
-          title: '第31回 計算工学講演会にて口頭発表を行いました',
+            },},{id: "news-第31回-計算工学講演会にて口頭発表-基調講演-を行いました",
+          title: '第31回 計算工学講演会にて口頭発表（基調講演）を行いました',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-06-05-presentation/";
@@ -161,6 +161,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-07-06-presentation/";
+            },},{id: "news-hors40th記念合同現地観測キャンペーンに参加しました",
+          title: 'HORS40th記念合同現地観測キャンペーンに参加しました',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2026-08-31-observation/";
             },},{id: "projects-高精度sph法",
           title: '高精度SPH法',
           description: "最小二乗SPH (LS-SPH) の開発",
